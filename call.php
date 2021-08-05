9e19205059169d8645cb4d91a9056fd31fc7f0d5
Pat Pannuto <pat.pannuto@gmail.com>
2021-08-05 09:31:00 -0700

    add CFP PDF and thumbnail

--- call.php
+++ call.php
@@ -4,9 +4,11 @@
 
 <p>
 	<div class="dates" style="float: right; margin: 0px 0px 10px 10px;">
+		<center>
 		<b>
-			<a href="files/cfp_2021.pdf"><img src="images/cfp.png" width="200px"><br><br>Download the CfP</a> 
+			<a href="files/cfp_2021.pdf"><img src="images/cfp_2021_thumb.jpg" width="200px"><br><br>Download the CfP</a> 
 		</b>
+		</center>
 		<h3>Important Dates</h3>
 		<?php print_important_dates(); ?>
 	</div>
